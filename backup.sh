dff3b2aa14a961933a3e272cd5f05b64522fb3f8
sda134 <60540655+sda134@users.noreply.github.com>
2021-03-29 10:11:38 +0900

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -17,34 +17,35 @@ echo "=== check file system ==="
 umount $target"2"
 sudo e2fsck -p -f -v $target"2"
 sudo resize2fs -P $target"2" > estimated_size.txt
-min_size=$(cut -d " " -f 7 estimated_size.txt)
+min_size_blocks=$(cut -d " " -f 7 estimated_size.txt)
 rm estimated_size.txt
-echo "[ estimated size is:$min_size (blocks) ]"
+echo "[ estimated size is:$min_size_blocks (4k; blocks) ]"
 
 
 echo
 echo "=== make fs small ==="
-new_fs_size_m=$((($min_size * 4 / 1024) + 100))
-new_fs_size_blocks=$((($new_fs_size_m * 1024)/4))   # 使わないが、メモとして残す
-echo "[ new fs size is:$new_fs_size_m M ]"
+new_fs_size_m=$((($min_size_blocks * 4 / 1024) + 100))      # used now
+new_fs_size_blocks=$((($new_fs_size_m * 1024)/4))           # used later for end sector of new partition.
+echo "[ new fs size is:$new_fs_size_m Mb ]"
 sudo resize2fs -p $target"2" $new_fs_size_m"M"
 
 
 echo
-echo "=== check the first sector of target ==="
+echo "=== check the first sector of the target ==="
 sudo fdisk -l $target | tail -n 1 | tr -s " " > fdisk_l.txt   # save the result of fdisk -l
 st_sector=$(cut -d " " -f 2 fdisk_l.txt )
 rm fdisk_l.txt
-echo "[ fist sector:$st_sector ]"   # 532480 
+echo "[ fist sector:$st_sector ]"
 
 
 echo
-echo "=== resize partition and check the last sector of target ==="
-partition_size_m=$(($new_fs_size_m + 100))
-echo "[ end point of 2nd partition:$partition_size_m M]"
+echo "=== resize partition and check the last sector of the target ==="
+new_fs_size_sector=$(((($new_fs_size_blocks * 4 * 1024) /512) + ((100 * 1024 * 1024)/ 512 ))) # added 100MB
+partition_end_sector=$(($new_fs_size_sector + $st_sector))
+echo "[ end point of 2nd partition:$partition_end_sector (512b; sectors) ]"
 
 sudo parted $target -s rm 2
-sudo parted $target -s mkpart primary $st_sector"s" $partition_size_m"M"
+sudo parted $target -s mkpart primary $st_sector"s" $partition_end_sector"s"
 
 sudo fdisk -l $target | tail -n 1 | tr -s " " > fdisk_l.txt   # save the result of fdisk -l
 from_last=$(cut -d " " -f 3 fdisk_l.txt )                     # sectro from last
